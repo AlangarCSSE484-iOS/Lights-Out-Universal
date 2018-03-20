387b4927d1172ad83073a4ed6dd98be4bf36a30d
Vibha <alangavr@rose-hulman.edu>
2018-03-20 01:32:33 -0400

    added code and connected things up - don't know why everything's broken

--- Lights Out Universal/ViewController.swift
+++ Lights Out Universal/ViewController.swift
@@ -10,20 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var gameBoardButtons: [UIButton]!
     
     @IBOutlet weak var gameStateLabel: UILabel!
     @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    var game = LinearLightsOutGame(numLights: 13)
+    var onLight = #imageLiteral(resourceName: "light_on")
+    var offLight = #imageLiteral(resourceName: "light_off")
     
     @IBAction func pressedNewGame(_ sender: Any) {
         print("You pressed new game!")
+        updateView()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         gameStateNavBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 20)]
-    }
+        //updateView()
         
+    }
     
-
+    @IBAction func gameButtonPressed(_ sender: Any) {
+        let gameBoardButtons = sender as! UIButton
+        print(gameBoardButtons.tag)
+        game.pressedLightAtIndex(gameBoardButtons.tag)
+        updateView()
+    }
+    
+    
+    func updateView() {
+        for i in 0..<13 {
+            let button = gameBoardButtons[i]
+            if (game.lightStates[i]){
+                button.setImage(onLight, for: UIControlState.normal)
+            } else {
+                button.setImage(offLight, for: UIControlState.normal)
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
